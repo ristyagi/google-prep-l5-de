8cc35742fdbe78c20c364783c80055ed3eb8e499
ristyagi <ristyagi@gmail.com>
2026-01-06 00:43:50 +0530

    added hashing basics.py

---
+++ 02_Patterns/01_Hashing/01_Hash_Map_basics.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 21:47:33 2026
+
+@author: Rishabh_Tyagi
+
+codebasics YT
+Hash Table - Data Structures & Algorithms Tutorials In Python #5
+https://www.youtube.com/watch?v=ea8BRGxGmlA&list=PLeo1K3hjS3uu_n_a__MI_KktGTLYopZ12&index=5
+
+"""
+
+"""
+Write a program that can give price based on a day from the csv
+99_data/stock_prices.csv
+"""
+
+# Using Array/List
+stock_prices = []
+with open('99_data/stock_prices.csv', 'r') as f:
+    for line in f:
+        day, price = line.strip().split(',')
+        stock_prices.append((day, float(price)))
+
+print(stock_prices)

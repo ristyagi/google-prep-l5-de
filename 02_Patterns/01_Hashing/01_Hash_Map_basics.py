8bfc7c9fd0f0bd48aa84b5c632cdca980d63ac24
ristyagi <ristyagi@gmail.com>
2026-01-06 13:59:39 +0530

    Added hashing.md and Hash_Map_basics.py files

--- 02_Patterns/01_Hashing/01_Hash_Map_basics.py
+++ 02_Patterns/01_Hashing/01_Hash_Map_basics.py
@@ -16,11 +16,51 @@ Write a program that can give price based on a day from the csv
 99_data/stock_prices.csv
 """
 
-# Using Array/List
-stock_prices = []
-with open('99_data/stock_prices.csv', 'r') as f:
-    for line in f:
-        day, price = line.strip().split(',')
-        stock_prices.append((day, float(price)))
+"""
+hashtable and hashmap are two terms used for that same datastrucutre and dictionary is the pyton implementation of this datastructure.
 
-print(stock_prices)
+in hashtable/hashmap, we use a hash function to get a 
+
+"""
+
+#####
+# Using Array/List
+
+stock_prices = []
+
+with open('99_data/stock_prices.csv', 'r', encoding="utf-8-sig") as f:
+    for line in f:
+        token = line.split(',')
+        day = token[0]
+        price = float(token[1])
+        stock_prices.append([day,price])
+
+stock_prices # its a list of lists, each sub list has two elements, date and price
+
+for element in stock_prices:
+    if element[0] == '06-Mar': # get price on 06-Mar
+        print(element[1])
+
+
+
+#####
+# Using Dictionary
+
+stock_price_dict = {}
+
+with open('99_data/stock_prices.csv', 'r', encoding="utf-8-sig") as fl:
+    for line in fl:
+        token = line.split(',')
+        day = token[0]
+        price = float(token[1])
+        stock_price_dict[day] = price
+
+stock_price_dict # dict with day as key, price as value
+
+stock_price_dict['06-Mar']
+stock_price_dict['11-Mar']
+
+
+
+
+
